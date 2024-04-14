ea55719fcf7d3e2817df319ce9dcd18a69beeb97
jakevvw <jakevvw@qq.com>
2024-04-14 17:56:48 +0800

    fix run error

--- shell_scripts/soft_link_creak.sh
+++ shell_scripts/soft_link_creak.sh
@@ -6,9 +6,11 @@ backup_file() {
 	local file_path=${1}
 
 	if [ -f "$file_path" ]; then
-		file_path=$(realpath "$file_path")
+		file_path="$file_path"
+		echo ${file_path}
 		timestamp=$(date +'%Y%m%d-%H%M%S')
-		local backup_file="${file_path}.${timestamp}"
+		local backup_file="${file_path}.${timestamp}.bak"
+		echo ${backup_file}
 		mv "${file_path}" "$backup_file"
 		echo "File backup to $backup_file"
 	else
@@ -18,25 +20,30 @@ backup_file() {
 
 detecte_zinit() {
 	local zinit_home="${HOME}/.local/share}/zinit"
-	if ! test -d "${zinit_home}"; then
+	if  test -d "${zinit_home}"; then
 		echo "Zinit exsit don't need install"
 	else
 		echo "zinit not installed"
 		echo "Will auto install"
-		source ./zinit_install.sh
+		source ./zinit_install.sh <<<n
 	fi
 }
 
 main() {
-	configs=("~/.zshrc")
+	configs=("${HOME}/.zshrc")
 	for config in "${configs[@]}"; do
+		echo "path:"
+		echo ${config}
 		backup_file "${config}"
 	done
-
+	
 	#insatll zinit
 	detecte_zinit
+
+	echo"==============================="
+	rm ${HOME}/.zshrc
 	#create zshrc soft link
-	ln -s "$(realpath "./zsh/zshrc")" $HOME/.zshrc
+	ln -s "$(realpath "../zsh/zshrc")" $HOME/.zshrc
 	if [ $? -eq 0 ]; then
 		echo "create zsh soft link ok"
 	else
--- shell_scripts/zinit_install.sh
+++ shell_scripts/zinit_install.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-bash -c "$(curl --fail --show-error --silent --location https://raw.githubusercontent.com/zdharma-continuum/zinit/HEAD/scripts/install.sh)"
-
+   bash -c "$(curl --fail --show-error --silent --location -x http://127.0.0.1:7890 https://raw.githubusercontent.com/zdharma-continuum/zinit/HEAD/scripts/install.sh)" <<< n
 zinit self-update
